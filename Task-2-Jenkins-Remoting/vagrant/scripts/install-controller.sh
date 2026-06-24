3646064f765b6b8bab75b7a6289c939740e0c85c
Ahmed Diaa <adiaa@localhost.localdomain>
2026-06-24 11:45:07 +0300

    Task-2-Jenkins-Remoting(Vagrant + VirtualBox)

---
+++ Task-2-Jenkins-Remoting/vagrant/scripts/install-controller.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+echo "Updating packages..."
+sudo apt update
+
+echo "Installing Java..."
+sudo apt install -y openjdk-17-jdk
+
+echo "Installing Jenkins..."
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key \
+| sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" \
+| sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update
+sudo apt install -y jenkins
+
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+echo "Jenkins Controller installation completed."
